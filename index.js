59be11d2036b3db87ce05d84aea550c9a09582e3
abodoma17 <o.a.ahmed3@student.aast.edu>
2024-10-31 13:13:44 +0300

    Initialized project

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+   res.json({
+       test:'test',
+       omar:123
+   });
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+})
