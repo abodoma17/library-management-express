7901f0a8f4f32e37d96f331e5a909277e07b2315
abodoma17 <o.a.ahmed3@student.aast.edu>
2024-11-01 21:46:18 +0200

    Added rate limiting to checkout and return endpoints

--- routes/borrowingRoutes.js
+++ routes/borrowingRoutes.js
@@ -3,8 +3,15 @@ const router = express.Router();
 const borrowingController = require('../controllers/borrowingController');
 const {check} = require('express-validator');
 const validateInputsMiddleware = require('../middlewares/validateInputsMiddleware');
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+    windowMs: 3 * 60 * 1000,
+    max: 10,
+});
 
 router.post('/borrowers/:borrowerID/checkout/:bookID', [
+        limiter,
         check('borrowerID').isNumeric().withMessage('Book ID should be valid number'),
         check('bookID').isNumeric().withMessage('Book ID should be valid number'),
         check('due_date').isISO8601().withMessage('Due date should be valid date'),
@@ -14,6 +21,7 @@ router.post('/borrowers/:borrowerID/checkout/:bookID', [
 );
 
 router.put('/borrowers/:borrowerID/return/:bookID', [
+        limiter,
         check('borrowerID').isNumeric().withMessage('Book ID should be valid number'),
         check('bookID').isNumeric().withMessage('Book ID should be valid number'),
         validateInputsMiddleware
